1226761b7d4ddbd5d5947001bac20811fd621cd2
Pratham Raut <prathamraut2323@gmail.com>
2025-11-26 19:47:13 +0000

    make attractive add after logo

--- src/components/site/Logo.tsx
+++ src/components/site/Logo.tsx
@@ -1,13 +1,17 @@
+import { Leaf } from 'lucide-react';
 import Link from 'next/link';
 
 export function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-3 group">
-      <div>
+    <Link href="/" className="flex items-center gap-2 group">
+      <div className="flex items-baseline">
         <div className="font-headline font-bold text-primary text-4xl leading-none">
           RAW
         </div>
-        <div className="text-sm text-muted-foreground leading-none mt-1 tracking-widest font-cursive">
+        <Leaf className="h-5 w-5 text-primary/80 ml-1" />
+      </div>
+      <div className="border-l pl-3 ml-1">
+        <div className="text-sm text-muted-foreground leading-tight tracking-widest font-cursive">
           Raut Wanojekar
         </div>
       </div>
