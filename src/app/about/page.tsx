95936b2a09e6ee4635455d378f114a320b1c8937
Pratham Raut <prathamraut2323@gmail.com>
2025-11-27 18:20:48 +0000

    remove that image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -31,10 +31,6 @@ const teamMembers = [
 
 const workImages = [
     {
-        src: "https://www.unisorting.com/wp-content/uploads/2021/04/orange-vision-3-header-mobile.jpg",
-        alt: "Orange sorting machine in action."
-    },
-    {
         src: "https://media.istockphoto.com/id/1180067245/photo/the-working-of-citrus-fruits.jpg?s=612x612&w=0&k=20&c=_5kfpVaDxGTwmVWUOYkcZrgQb3mzKVrJ246KYHzBxTQ=",
         alt: "Citrus fruits being processed on a conveyor belt."
     },
